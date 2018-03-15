7ba58071543d2e54b2839f079b8d31fdd3236beb
Matt Clough <matt.clough@mobelux.com>
2018-03-15 13:53:33 -0400

    add dependencies, basic gatsby-source-morty config, etc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Default Starter'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-morty',
+      options: {
+        account: process.env.MORTY_ACCOUNT,
+        accessToken: process.env.MORTY_TOKEN
+      }
+    }
+  ]
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,138 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
+const introspectionQuery = require('graphql/utilities/introspectionQuery');
+const fetchSchema = require('graphql-js-schema-fetch').default;
 
- // You can delete this file if you're not using it
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators;
+
+  const pages = new Promise((resolve, reject) => {
+    graphql(
+      `
+        {
+          allMortyPage {
+            edges {
+              node {
+                template
+                id
+                route
+              }
+            }
+          }
+        }
+      `
+    ).then(result => {
+      if (result.errors) {
+        console.log(result.errors);
+        reject(result.errors);
+      }
+
+      result.data.allMortyPage.edges.forEach(edge => {
+        if (edge.node.template && edge.node.template.length) {
+          const template = path.resolve(
+            'src',
+            'templates',
+            `${edge.node.template}.js`
+          );
+
+          createPage({
+            path: edge.node.route,
+            component: template,
+            context: {
+              id: edge.node.id
+            }
+          });
+        }
+      });
+      resolve();
+    });
+  });
+
+  const stories = new Promise((resolve, reject) => {
+    graphql(
+      `
+        {
+          allMortyStory {
+            edges {
+              node {
+                template
+                id
+                route
+              }
+            }
+          }
+        }
+      `
+    ).then(result => {
+      if (result.errors) {
+        console.log(result.errors);
+        reject(result.errors);
+      }
+
+      result.data.allMortyStory.edges.forEach((edge, ix) => {
+        const prevId =
+          (ix > 0 && edges[ix - 1].node.id) || edges[edges.length - 1].node.id;
+        const nextId =
+          (ix < edges.length - 1 && edges[ix + 1].node.id) || edges[0].node.id;
+
+        const template = path.resolve(
+          'src',
+          'templates',
+          `${edge.node.template}.js`
+        );
+
+        createPage({
+          path: edge.node.route,
+          component: template,
+          context: {
+            id: edge.node.id,
+            prevId: prevId,
+            nextId: nextId
+          }
+        });
+      });
+      resolve();
+    });
+  });
+
+  const events = new Promise((resolve, reject) => {
+    graphql(
+      `
+        {
+          allMortyEvent {
+            edges {
+              node {
+                template
+                id
+                route
+              }
+            }
+          }
+        }
+      `
+    ).then(result => {
+      if (result.errors) {
+        console.log(result.errors);
+        reject(result.errors);
+      }
+
+      result.data.allMortyEvent.edges.forEach(edge => {
+        const template = path.resolve(
+          'src',
+          'templates',
+          `${edge.node.template}.js`
+        );
+
+        createPage({
+          path: edge.node.route,
+          component: template,
+          context: {
+            id: edge.node.id
+          }
+        });
+      });
+      resolve();
+    });
+  });
+
+  return Promise.all([pages, events, stories]);
+};
